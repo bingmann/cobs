0198041d530c23e08be7d44bdde511144826f4e7
Florian Gauger <flo.gauger@gmail.com>
2017-09-26 18:12:19 +0200

    Reformat CMakeLists.txt

---
+++ dependencies/dependency.cmake
@@ -0,0 +1,34 @@
+set(DEPENDENCY_DIR ${CMAKE_BINARY_DIR}/dependencies)
+
+# Download and unpack googletest at configure time
+configure_file(dependencies/gtest.cmake dependencies/gtest/download/CMakeLists.txt)
+execute_process(COMMAND ${CMAKE_COMMAND} -G "${CMAKE_GENERATOR}" .
+        RESULT_VARIABLE result
+        WORKING_DIRECTORY ${DEPENDENCY_DIR}/gtest/download)
+if(result)
+    message(FATAL_ERROR "CMake step for googletest failed: ${result}")
+endif()
+execute_process(COMMAND ${CMAKE_COMMAND} --build .
+        RESULT_VARIABLE result
+        WORKING_DIRECTORY ${DEPENDENCY_DIR}/gtest/download)
+if(result)
+    message(FATAL_ERROR "Build step for googletest failed: ${result}")
+endif()
+add_subdirectory(${DEPENDENCY_DIR}/gtest/src ${DEPENDENCY_DIR}/gtest/build)
+
+
+configure_file(dependencies/xxhash.cmake dependencies/xxhash/download/CMakeLists.txt)
+execute_process(COMMAND ${CMAKE_COMMAND} -G "${CMAKE_GENERATOR}" .
+        RESULT_VARIABLE result
+        WORKING_DIRECTORY ${DEPENDENCY_DIR}/xxhash/download)
+if(result)
+    message(FATAL_ERROR "CMake step for xxhash failed: ${result}")
+endif()
+execute_process(COMMAND ${CMAKE_COMMAND} --build .
+        RESULT_VARIABLE result
+        WORKING_DIRECTORY ${DEPENDENCY_DIR}/xxhash/download)
+if(result)
+    message(FATAL_ERROR "Build step for xxhash failed: ${result}")
+endif()
+add_subdirectory(${DEPENDENCY_DIR}/xxhash/src/cmake_unofficial)
+
