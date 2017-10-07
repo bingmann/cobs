94862c5db561f8a427e5ddf1b47c0adb8da4d34c
Florian Gauger <flo.gauger@gmail.com>
2017-10-07 16:47:07 +0200

    Fix bug in frequency

--- src/main_frequency.cpp
+++ src/main_frequency.cpp
@@ -3,9 +3,9 @@
 int main(int argc, char** argv) {
     if (argc == 4) {
         char* pEnd;
-        std::string in_dir = argv[2];
-        std::string out_dir = argv[3];
-        size_t batch_size = std::strtoul(argv[1], &pEnd, 10);
+        std::string in_dir = argv[1];
+        std::string out_dir = argv[2];
+        size_t batch_size = std::strtoul(argv[3], &pEnd, 10);
         genome::frequency::process_all_in_directory<genome::frequency::bin_pq_element>(in_dir, out_dir, batch_size);
     } else {
         std::cout << "wrong number of args: in_dir out_dir batch_size" << std::endl;
