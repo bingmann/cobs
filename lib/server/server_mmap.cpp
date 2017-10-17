c64a76e410454d4426fe97fac89e7786aa0f094c
Florian Gauger <flo.gauger@gmail.com>
2017-10-17 11:23:12 +0200

    Fix bug in server_mmap

--- lib/server/server_mmap.cpp
+++ lib/server/server_mmap.cpp
@@ -19,91 +19,69 @@ namespace genome {
 //  | MAP_POPULATE
         m_data = start_pos + reinterpret_cast<byte*>(mmap(NULL, size, PROT_READ, MAP_PRIVATE, fd, 0));
     }
-/*
-    void server_mmap::get_counts(const std::vector<size_t>& hashes, std::vector<uint16_t>& counts) {
-        std::vector<byte> count(m_bfh.block_size(), 0xFF);
-        auto* count_64 = reinterpret_cast<uint64_t*>(count.data());
-        auto* counts_64 = reinterpret_cast<uint64_t*>(counts.data());
 
-        std::vector<char> count_1(m_bfh.block_size() * hashes.size(), 'a');
-        int a = 0;
-        for (size_t i = 0; i < hashes.size(); i++) {
-            m_timer.active("mmap_access");
-            auto data_64 = reinterpret_cast<uint64_t*>(m_data + hashes[i] * m_bfh.block_size());
-            size_t j = 1;
-            while (j * 8 <= m_bfh.block_size()) {
-                count_64[j - 1] &= data_64[j - 1];
-                j++;
-            }
-            j = (j - 1) * 8;
-            while (j < m_bfh.block_size()) {
-                count[j] &= m_data[hashes[i] * m_bfh.block_size() + j];
-                j++;
-            }
-            if (i % m_bfh.num_hashes() == m_bfh.num_hashes() - 1) {
-                m_timer.active("compute_counts");
-                for (size_t k = 0; k < m_bfh.block_size(); k++) {
-                    counts_64[2 * k] += m_count_expansions[count[k] & 0xF];
-                    counts_64[2 * k + 1] += m_count_expansions[count[k] >> 4];
-                }
-                std::fill(count.begin(), count.end(), 0xFF);
-            }
-        }
-    }
-    */
     void server_mmap::read_from_disk(const std::vector<size_t>& hashes, char* rows) {
-        m_timer.active("mmap_access");
         #pragma omp parallel for
         for (size_t i = 0; i < hashes.size(); i++) {
-            auto data_64 = reinterpret_cast<uint64_t*>(m_data + hashes[i] * m_bfh.block_size());
-            auto rows_64 = reinterpret_cast<uint64_t*>(rows + i * m_bfh.block_size());
-            size_t j = 1;
-            while (j * 8 <= m_bfh.block_size()) {
-                rows_64[j - 1] = data_64[j - 1];
+            auto data_8 = m_data + hashes[i] * m_bfh.block_size();
+            auto rows_8 = rows + i * m_bfh.block_size();
+            auto data_64 = reinterpret_cast<uint64_t*>(data_8);
+            auto rows_64 = reinterpret_cast<uint64_t*>(rows_8);
+            size_t j = 0;
+            while ((j + 1) * 8 <= m_bfh.block_size()) {
+                rows_64[j] = data_64[j];
                 j++;
             }
-            j = (j - 1) * 8;
+            j *= 8;
             while (j < m_bfh.block_size()) {
-                rows[i * m_bfh.block_size() + j] = m_data[j];
+                rows_8[j] = data_8[j];
                 j++;
             }
         }
     }
 
-    void server_mmap::get_counts(const std::vector<size_t>& hashes, std::vector<uint16_t>& counts) {
-        std::vector<byte> count(m_bfh.block_size(), 0xFF);
-        auto* count_64 = reinterpret_cast<uint64_t*>(count.data());
-        auto* counts_64 = reinterpret_cast<uint64_t*>(counts.data());
-
-        std::vector<char> rows(m_bfh.block_size() * hashes.size());
-        read_from_disk(hashes, rows.data());
-
-        m_timer.active("compute_counts");
+    void server_mmap::aggregate_rows(const std::vector<size_t>& hashes, std::vector<char>& rows) {
         #pragma omp parallel for
         for (uint64_t i = 0; i < hashes.size(); i += m_bfh.num_hashes()) {
-            for (size_t j = 0; j < m_bfh.num_hashes(); j++) {
-                char* rows_8 = rows.data() + (i + j) * m_bfh.block_size();
-                auto rows_64 = reinterpret_cast<uint64_t*>(rows_8);
-                size_t k = 1;
-                while (k * 8 <= m_bfh.block_size()) {
-                    count_64[k - 1] &= rows_64[k - 1];
+            auto rows_8 = rows.data() + i * m_bfh.block_size();
+            auto rows_64 = reinterpret_cast<uint64_t*>(rows_8);
+            for (size_t j = 1; j < m_bfh.num_hashes(); j++) {
+                auto rows_8_j = rows_8 + j * m_bfh.block_size();
+                auto rows_64_j = reinterpret_cast<uint64_t*>(rows_8_j);
+                size_t k = 0;
+                while ((k + 1) * 8 <= m_bfh.block_size()) {
+                    rows_64[k] &= rows_64_j[k];
                     k++;
                 }
-                k = (k - 1) * 8;
+                k = k * 8;
                 while (k < m_bfh.block_size()) {
-                    count[k] &= rows_8[k];
+                    rows_8[k] &= rows_8_j[k];
                     k++;
-                }
-                if (j + 1 == m_bfh.num_hashes()) {
-                    for (size_t k = 0; k < m_bfh.block_size(); k++) {
-                        counts_64[2 * k] += m_count_expansions[count[k] & 0xF];
-                        counts_64[2 * k + 1] += m_count_expansions[count[k] >> 4];
-//                    counts_64[2 * k] += m_count_expansions[count_1[i * hashes.size() + k] & 0xF];
-//                    counts_64[2 * k + 1] += m_count_expansions[count_1[i * hashes.size() + k] >> 4];
-                    }
-                    std::fill(count.begin(), count.end(), 0xFF);
                 }
             }
         }
+    }
+
+    void server_mmap::compute_counts(const std::vector<size_t>& hashes, std::vector<uint16_t>& counts, const std::vector<char>& rows) {
+        auto counts_64 = reinterpret_cast<uint64_t*>(counts.data());
+        #pragma omp parallel for
+        for (uint64_t i = 0; i < hashes.size(); i += m_bfh.num_hashes()) {
+            auto rows_8 = rows.data() + i * m_bfh.block_size();
+            for (size_t k = 0; k < m_bfh.block_size(); k++) {
+                counts_64[2 * k] += m_count_expansions[rows_8[k] & 0xF];
+                counts_64[2 * k + 1] += m_count_expansions[rows_8[k] >> 4];
+            }
+        }
+    }
+
+    void server_mmap::get_counts(const std::vector<size_t>& hashes, std::vector<uint16_t>& counts) {
+        std::vector<char> rows(m_bfh.block_size() * hashes.size());
+        m_timer.active("mmap_access");
+        read_from_disk(hashes, rows.data());
+        m_timer.active("aggregate_rows");
+        aggregate_rows(hashes, rows);
+        m_timer.active("compute_counts");
+        compute_counts(hashes, counts, rows);
+        //todo test if it is faster to combine these functions for better cache locality
     }
 }
--- lib/server/server_mmap.hpp
+++ lib/server/server_mmap.hpp
@@ -8,6 +8,8 @@ namespace genome {
     private:
         byte* m_data;
         void read_from_disk(const std::vector<size_t>& hashes, char* rows);
+        void aggregate_rows(const std::vector<size_t>& hashes, std::vector<char>& rows);
+        void compute_counts(const std::vector<size_t>& hashes, std::vector<uint16_t>& counts, const std::vector<char>& rows);
     protected:
         void get_counts(const std::vector<size_t>& hashes, std::vector<uint16_t>& counts) override;
     public:
