9aa655cabd5d869ba319a1fe420cc1a226c4c5d1
Florian Gauger <flo.gauger@gmail.com>
2017-11-25 11:12:26 +0100

    Remove stxxl

--- dependencies/dependency.cmake
+++ dependencies/dependency.cmake
@@ -47,18 +47,3 @@ if(result)
 endif()
 add_subdirectory(${DEPENDENCY_DIR}/cli11/src)
 
-configure_file(dependencies/stxxl.cmake dependencies/stxxl/download/CMakeLists.txt)
-execute_process(COMMAND ${CMAKE_COMMAND} -G "${CMAKE_GENERATOR}" .
-        RESULT_VARIABLE result
-        WORKING_DIRECTORY ${DEPENDENCY_DIR}/stxxl/download)
-if(result)
-    message(FATAL_ERROR "CMake step for stxxl failed: ${result}")
-endif()
-execute_process(COMMAND ${CMAKE_COMMAND} --build .
-        RESULT_VARIABLE result
-        WORKING_DIRECTORY ${DEPENDENCY_DIR}/stxxl/download)
-if(result)
-    message(FATAL_ERROR "Build step for stxxl failed: ${result}")
-endif()
-add_subdirectory(${DEPENDENCY_DIR}/stxxl/src)
-
