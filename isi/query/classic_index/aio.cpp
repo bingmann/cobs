7a89ce7792b7e32d8be70fb10839824f6a23c486
Florian Gauger <flo.gauger@gmail.com>
2017-12-13 00:14:14 +0100

    Move aio code into initialization

--- isi/query/classic_index/aio.cpp
+++ isi/query/classic_index/aio.cpp
@@ -8,8 +8,16 @@
 
 namespace isi::query::classic_index {
 
-    aio::aio(const std::experimental::filesystem::path& path) : classic_index::base(path) {
+    aio::aio(const std::experimental::filesystem::path& path) : classic_index::base(path), m_iocbs(65536, iocb()), m_iocbpp(65536) {
         m_fd = open_file(path);
+        
+        for (size_t i = 0; i < hashes.size(); i++) {
+            m_iocbs[i].aio_fildes = m_fd;
+            m_iocbs[i].aio_lio_opcode = IOCB_CMD_PREAD;
+            m_iocbs[i].aio_offset = hashes[i] % m_header.signature_size() * m_header.block_size();
+            m_iocbs[i].aio_nbytes = m_header.block_size();
+            m_iocbpp[i] = m_iocbs.data() + i;
+        }
     }
 
     aio::~aio() {
@@ -17,6 +25,8 @@ namespace isi::query::classic_index {
     }
 
     void aio::read_from_disk(const std::vector<size_t>& hashes, char* rows) {
+        timer t;
+        t.active("setup");
         aio_context_t ctx;
         io_event events[hashes.size()];
         int ret;
@@ -27,38 +37,32 @@ namespace isi::query::classic_index {
             exit_error_errno("io_setup error");
         }
 
-        std::vector<iocb> iocbs(hashes.size(), iocb());
-        std::vector<iocb*> iocbpp;
-
+        #pragma omp parallel for
         for (size_t i = 0; i < hashes.size(); i++) {
-            iocbs[i].aio_fildes = m_fd;
-            iocbs[i].aio_lio_opcode = IOCB_CMD_PREAD;
-            iocbs[i].aio_buf = (uint64_t) rows + i * m_header.block_size();
-            iocbs[i].aio_offset = hashes[i] % m_header.signature_size() * m_header.block_size();
-            iocbs[i].aio_nbytes = m_header.block_size();
-            iocbpp.push_back(iocbs.data() + i);
+            m_iocbs[i].aio_buf = (uint64_t) rows + i * m_header.block_size();
+            m_iocbs[i].aio_offset = hashes[i] % m_header.signature_size() * m_header.block_size();
         }
 
-        std::cout << "aio_nbytes: " << iocbpp[0][0].aio_buf << std::endl;
-        std::cout << "aio_nbytes: " << iocbpp[1][0].aio_buf << std::endl;
-
-        ret = io_submit(ctx, iocbpp.size(), iocbpp.data());
-        if (ret != static_cast<int64_t>(iocbpp.size())) {
+        ret = io_submit(ctx, hashes.size(), m_iocbpp.data());
+        t.active("io");
+        if (ret != static_cast<int64_t>(hashes.size())) {
             if (ret < 0)
                 perror("io_submit error");
             else
                 fprintf(stderr, "could not sumbit IOs");
             exit_error_errno("io_submit error");
         }
-        std::cout << "after submit" << std::endl;
 
-        /* get the reply */
-        ret = io_getevents(ctx, iocbpp.size(), iocbpp.size(), events, nullptr);
-        std::cout << "ret: " << ret << std::endl;
+        ret = io_getevents(ctx, hashes.size(), hashes.size(), events, nullptr);
+        if (ret < hashes.size()) {
+            exit_error_errno("io_getevents error");
+        }
 
         ret = io_destroy(ctx);
         if (ret < 0) {
             exit_error_errno("io_destroy error");
         }
+        t.stop();
+        std::cout << t << std::endl;
     }
 }
--- isi/query/classic_index/aio.hpp
+++ isi/query/classic_index/aio.hpp
@@ -6,6 +6,8 @@ namespace isi::query::classic_index {
     class aio : public base {
     private:
         int m_fd;
+        std::vector<iocb> m_iocbs;
+        std::vector<iocb*> m_iocbpp;
     protected:
         virtual void read_from_disk(const std::vector<size_t>& hashes, char* rows);
     public:
