ac71dd8fc7f4a58e0b6536ea233eb1fc77407e57
Florian Gauger <flo.gauger@gmail.com>
2017-12-13 16:18:11 +0100

    Fix compact aio query

--- isi/query/compact_index/aio.cpp
+++ isi/query/compact_index/aio.cpp
@@ -8,8 +8,7 @@
 #include <fcntl.h>
 
 namespace isi::query::compact_index {
-
-    aio::aio(const std::experimental::filesystem::path& path) : compact_index::base(path) {
+    aio::aio(const std::experimental::filesystem::path& path) : compact_index::base(path), m_ctx(0) {
         assert_exit(m_header.page_size() % isi::get_page_size() == 0, "page size needs to be divisible by 4096 so the index can be opened with O_DIRECT");
 
         m_offsets.resize(m_header.parameters().size());
@@ -19,9 +18,7 @@ namespace isi::query::compact_index {
         }
 
         m_fd = open_file(path, O_RDONLY | O_DIRECT);
-        m_ctx = 0;
-        int ret = io_setup(65536, &m_ctx);
-        if (ret < 0) {
+        if (io_setup(65536, &m_ctx) < 0) {
             exit_error_errno("io_setup error");
         }
 
@@ -35,46 +32,51 @@ namespace isi::query::compact_index {
 
     aio::~aio() {
         close_file(m_fd);
-        int ret = io_destroy(m_ctx);
-        if (ret < 0) {
+        if (io_destroy(m_ctx) < 0) {
             exit_error_errno("io_destroy error");
         }
     }
 
     void aio::read_from_disk(const std::vector<size_t>& hashes, char* rows) {
         timer t;
-        io_event events[hashes.size()];
-        int ret;
+        int64_t num_requests = m_header.parameters().size() * hashes.size();
 
         t.active("setup");
         #pragma omp parallel for collapse(2)
         for (size_t i = 0; i < m_header.parameters().size(); i++) {
             for (size_t j = 0; j < hashes.size(); j++) {
+                uint64_t index = i + j * m_header.parameters().size();
                 uint64_t hash = hashes[j] % m_header.parameters()[i].signature_size;
-                m_iocbs[i].aio_buf = (uint64_t) rows + (i + j * m_header.parameters().size()) * m_header.page_size();
-                m_iocbs[i].aio_offset = m_offsets[i] + hash * m_header.page_size();
+                //todo rows does not need to be reallocated each time
+                m_iocbs[index].aio_buf = (uint64_t) rows + index * m_header.page_size();
+                m_iocbs[index].aio_offset = m_offsets[i] + hash * m_header.page_size();
             }
         }
 
-        int64_t num_requests = m_header.parameters().size() * hashes.size();
-
         t.active("io_submit");
-        ret = io_submit(m_ctx, num_requests, m_iocbpp.data());
-        if (ret != static_cast<int64_t>(num_requests)) {
-            if (ret < 0)
+        int ret = io_submit(m_ctx, num_requests, m_iocbpp.data());
+        if (ret != num_requests) {
+            if (ret < 0) {
                 perror("io_submit error");
-            else
+            } else {
                 fprintf(stderr, "could not sumbit IOs");
+            }
             exit_error_errno("io_submit error");
         }
 
         t.active("io_getevents");
-        ret = io_getevents(m_ctx, num_requests, num_requests, events, nullptr);
-        if (ret < num_requests) {
+        io_event events[num_requests];
+        if (io_getevents(m_ctx, num_requests, num_requests, events, nullptr) < num_requests) {
             exit_error_errno("io_getevents error");
         }
+
+        for(size_t i = 0; i < num_requests; i++) {
+            if(events[i].res != m_header.page_size()) {
+                std::cout << i << " " << std::strerror(-events[i].res) << std::endl;
+            }
+        }
+
         t.stop();
         std::cout << t << std::endl;
     }
 }
-
