33c766ffa551041312ce653fcfce3fa9e9dec782
Florian Gauger <flo.gauger@gmail.com>
2017-12-15 10:48:04 +0100

    Add linux aio tests

--- test/src/compact_index_query.cpp
+++ test/src/compact_index_query.cpp
@@ -24,7 +24,7 @@ namespace {
         }
     };
 
-    TEST_F(compact_index_query, all_included) {
+    TEST_F(compact_index_query, all_included_mmap) {
         auto samples = generate_samples_all(query);
         generate_test_case(samples, tmp_dir);
         isi::compact_index::create_folders(tmp_dir, in_dir, 2);
@@ -40,7 +40,7 @@ namespace {
         }
     }
 
-    TEST_F(compact_index_query, one_included) {
+    TEST_F(compact_index_query, one_included_mmap) {
         auto samples = generate_samples_one(query);
         generate_test_case(samples, tmp_dir);
         isi::compact_index::create_folders(tmp_dir, in_dir, 2);
@@ -55,7 +55,7 @@ namespace {
         }
     }
 
-    TEST_F(compact_index_query, false_positive) {
+    TEST_F(compact_index_query, false_positive_mmap) {
         auto samples = generate_samples_all(query);
         generate_test_case(samples, tmp_dir);
         isi::compact_index::create_folders(tmp_dir, in_dir, 2);
@@ -79,4 +79,62 @@ namespace {
             ASSERT_LE(np.second, 1070U);
         }
     }
+
+#ifdef __linux__
+    TEST_F(compact_index_query, all_included_aio) {
+        auto samples = generate_samples_all(query);
+        generate_test_case(samples, tmp_dir);
+        isi::compact_index::create_folders(tmp_dir, in_dir, 2);
+        isi::compact_index::create_from_folders(in_dir, 8, 3, 0.1, 2);
+        isi::query::compact_index::aio s_aio(index_path);
+
+        std::vector<std::pair<uint16_t, std::string>> result;
+        s_aio.search(query, 31, result);
+        ASSERT_EQ(samples.size(), result.size());
+        for(auto& r: result) {
+            int index = std::stoi(r.second.substr(r.second.size() - 2));
+            ASSERT_GE(r.first, samples[index].data().size());
+        }
+    }
+
+    TEST_F(compact_index_query, one_included_aio) {
+        auto samples = generate_samples_one(query);
+        generate_test_case(samples, tmp_dir);
+        isi::compact_index::create_folders(tmp_dir, in_dir, 2);
+        isi::compact_index::create_from_folders(in_dir, 8, 3, 0.1, 2);
+        isi::query::compact_index::mmap s_aio(index_path);
+
+        std::vector<std::pair<uint16_t, std::string>> result;
+        s_aio.search(query, 31, result);
+        ASSERT_EQ(samples.size(), result.size());
+        for(auto& r: result) {
+            ASSERT_EQ(r.first, 1);
+        }
+    }
+
+    TEST_F(compact_index_query, false_positive_aio) {
+        auto samples = generate_samples_all(query);
+        generate_test_case(samples, tmp_dir);
+        isi::compact_index::create_folders(tmp_dir, in_dir, 2);
+        isi::compact_index::create_from_folders(in_dir, 8, 3, 0.1, 2);
+        isi::query::compact_index::mmap s_aio(index_path);
+
+        size_t num_tests = 10000;
+        std::map<std::string, uint64_t> num_positive;
+        std::vector<std::pair<uint16_t, std::string>> result;
+        for (size_t i = 0; i < num_tests; i++) {
+            std::string query_2 = isi::random_sequence(31, i);
+            s_aio.search(query_2, 31, result);
+
+            for (auto& r: result) {
+                num_positive[r.second] += r.first;
+                ASSERT_TRUE(r.first == 0 || r.first == 1);
+            }
+        }
+
+        for (auto& np: num_positive) {
+            ASSERT_LE(np.second, 1070U);
+        }
+    }
+#endif
 }
