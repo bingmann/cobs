eda0facdd1e841e5dbecfcc646a352d7f28fbec7
Florian Gauger <flo.gauger@gmail.com>
2018-01-11 15:46:16 +0100

    Add fp_dist experiment

--- src/exp_fp_dist.cpp
+++ src/exp_fp_dist.cpp
@@ -1,27 +1,34 @@
 #include <cstddef>
 #include <cstdint>
 #include <map>
+#include <string>
 #include <vector>
-#include <isi/query/compact_index/aio.hpp>
+#include <isi/query/compact_index/mmap.hpp>
+
 
 int main(int argc, char **argv) {
-    std::string index = "/ssd1/ena_4096.com_idx.isi";
+    std::array<uint32_t, 6> indices = {4096,  8192, 12288, 16384, 28672, 57344};
+
     std::string sample_name = "ERR498185";
     std::vector<std::string> queries;
-    for (size_t i = 0; i < 100000; i++) {
-        queries.push_back(isi::random_sequence(1030, (size_t) std::rand()));
+    for (size_t i = 0; i < 1; i++) {
+        queries.push_back(isi::random_sequence(1030, (size_t) time(nullptr)));
     }
-    isi::query::compact_index::aio s(index);
-    std::map<size_t, size_t> counts;
     std::vector<std::pair<uint16_t, std::string>> result;
-    for (size_t i = 0; i < queries.size(); i++) {
-        s.search(queries[i], 31, result);
-        for (const auto& r: result) {
-            if (r.second == sample_name) {
-                std::cout << r.first << std::endl;
-                break;
+
+    for (uint32_t in: indices) {
+        std::string index = "/well/iqbal/people/florian/indices/ena_" + std::to_string(in) +".com_idx.isi";
+
+        std::map<size_t, size_t> counts;
+        isi::query::compact_index::mmap s(index);
+        for (size_t i = 0; i < queries.size(); i++) {
+            s.search(queries[i], 31, result);
+            for (const auto& r: result) {
+                counts[r.first]++;
             }
         }
+        for (auto a: counts) {
+            std::cout << a.first << "," << a.second << "," << in << "\n";
+        }
     }
-
 }
