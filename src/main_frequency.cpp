affdae669c595fc1a018d9dc8dd416de4d7879cf
Timo Bingmann <tbgit@panthema.net>
2018-10-18 08:17:41 +0200

    Revert "Remove code for experiments"
    This reverts commit 7c56ab0a9d0238e8a7c96aff7271ae4204b4c66a.

---
+++ src/main_frequency.cpp
@@ -0,0 +1,29 @@
+#include <isi/frequency.hpp>
+
+int main(int argc, char** argv) {
+    if (argc == 5) {
+        char* pEnd;
+        std::string type = argv[1];
+        std::string in_dir = argv[2];
+        std::string out_dir = argv[3];
+        size_t batch_size = std::strtoul(argv[4], &pEnd, 10);
+        if (type == "bin") {
+            isi::frequency::process_all_in_directory<isi::file::sample_header>(in_dir, out_dir, batch_size);
+        } else if (type == "fre") {
+            isi::frequency::process_all_in_directory<isi::file::frequency_header>(in_dir, out_dir, batch_size);
+        } else {
+            std::cout << "wrong type: use 'bin' or 'fre'" << std::endl;
+        }
+    } else if (argc == 4) {
+        std::string type = argv[1];
+        std::string in_file = argv[2];
+        std::string out_file = argv[3];
+        if (type == "com") {
+            isi::frequency::combine(in_file, out_file);
+        } else {
+            std::cout << "wrong type: use 'com'" << std::endl;
+        }
+    } else {
+        std::cout << "wrong number of args:\n\ttype[bin/fre] in_dir out_dir batch_size\n\ttype[com] in_file out_file" << std::endl;
+    }
+}
