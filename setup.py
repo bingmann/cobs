b155cbb88cb279065cff6251fafe5e047a135221
Timo Bingmann <tbgit@panthema.net>
2019-11-06 13:37:17 +0100

    build pypi package cobs_index v0.1.2

--- setup.py
+++ setup.py
@@ -77,7 +77,7 @@ with open("README.md", "r") as fh:
 if __name__ == '__main__':
     setup(
         name='cobs_index',
-        version='0.1.1',
+        version='0.1.2',
         description='Compact Bit-Sliced Signature Index (COBS)',
         long_description=long_description,
         long_description_content_type="text/markdown",
